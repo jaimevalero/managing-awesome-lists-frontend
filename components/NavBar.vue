553b0b39cc5b987cc360bf2b8968cfc4c38465ae
jaimevalero <jaime.valeromarquez@telefonica.com>
2024-01-30 18:57:39 +0100

    Add jira_board_github_repos

---
+++ components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar class="app-bar-color" color="info-darken-1" density="compact" >
+    <v-app-bar-nav-icon @click="$emit('toggle-drawer')"></v-app-bar-nav-icon>
+    <v-toolbar-title  class="mr-auto">
+      <div class="nav-brand">Awesome List Viewer</div>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-row no-gutters>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DropdownMenu from '../components/DropdownMenu.vue'
+
+export default defineComponent({
+  components: {
+    DropdownMenu
+  }
+})
+</script>
+
+<style scoped>
+.nav-brand {
+  font-weight: bold;
+  padding: 2px 0;
+}
+.app-bar-color {
+  background-color: 'info-darken-1' !important; /* staticPrimaryColor de tu archivo themes.ts */
+  color: 'info-darken-1' !important;
+}
+</style>
